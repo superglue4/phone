78b4eec02ff1122e1ed52ad7ed8944725a451242
sjr4746 <sjr4746@bubblecon.io>
2026-04-21 10:44:46 +0900

    Initial Commit

---
+++ tracker/trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackerApp.swift
+//  tracker
+//
+//  Created by 신정열 on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
